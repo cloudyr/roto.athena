46c49fb12aaf18be9c8639e5cf8fffee3af44d2d
zach charlop-powers <zach.charlop.powers@gmail.com>
2018-09-07 17:02:27 -0400

    execute and save query

--- R/execute-and-save-query.R
+++ R/execute-and-save-query.R
@@ -11,6 +11,7 @@
 #' @param query SQL query statements to be executed
 #' @param database database within which the query executes.
 #' @param output_location location in S3 where query results are stored.
+#' @param local_filename download location of the athena result
 #' @param client_request_token unique case-sensitive string used to ensure the
 #'        request to create the query is idempotent (executes only once). If another
 #'        `StartQueryExecution` request is received, the same response is returned
@@ -26,18 +27,15 @@
 #' @param aws_secret_access_key AWS secret access key
 #' @param aws_session_token AWS session token
 #' @param region_name region name
-#' @param profile_name profile name
-#' @param query_execution_id unique ID of the query execution.
-#' @param filename download location of the athena result
-#' @param aws_access_key_id AWS access key id
-#' @param aws_secret_access_key AWS secret access key
-#' @param aws_session_token AWS session token
-#' @param region_name region name
-#' @param profile_name profile name
+#' @param profile profile name
+#' @param polling_duration how often to check for results
+#' @param max_time how long to keep looking for data
+#' @param checktime whether to check for time
 #' @return the filename
 #' @references <https://boto3.readthedocs.io/en/latest/reference/services/athena.html#Athena.Client.get_query_results>
 #' @importFrom future future
 #' @importFrom future resolved
+#' @importFrom future value
 #' @export
 #' @examples \dontrun{
 #' execute_and_save_query(
@@ -49,23 +47,122 @@
 #' }
 #'
 #'
-execute_and_save_query <- function(query_execution_id,
-                               filename,
-                               aws_access_key_id = NULL,
-                               aws_secret_access_key = NULL,
-                               aws_session_token = NULL,
-                               region_name = NULL,
-                               profile_name = NULL) {
+execute_and_save_query <- function(query,
+                                   database,
+                                   output_location,
+                                   local_filename,
+                                   client_request_token = NULL,
+                                   encryption_option = NULL,
+                                   kms_key = NULL,
+                                   aws_access_key_id = NULL,
+                                   aws_secret_access_key = NULL,
+                                   aws_session_token = NULL,
+                                   region_name = NULL,
+                                   profile_name = NULL,
+                                   polling_duration = 15,
+                                   checktime=FALSE,
+                                   maxtime=1000
+                                   ) {
 
-  executionid <- start_query_execution()
-  localfile   <- future(save_query_results())
+  # create the query and obtain an executionID
+  executionid <- start_query_execution(
+    query=query,
+    database=database,
+    output_location=output_location,
+    client_request_token = uuid::UUIDgenerate(),
+    encryption_option = encryption_option,
+    kms_key = kms_key,
+    aws_access_key_id = aws_access_key_id,
+    aws_secret_access_key = aws_secret_access_key,
+    aws_session_token = aws_session_token,
+    region_name = region_name,
+    profile_name = profile_name
+  )
 
-  value(localfile)
+  # use the execution ID to obtain the S3 path where the output data will be
+  boto3$session$Session(
+    aws_access_key_id = aws_access_key_id,
+    aws_secret_access_key = aws_secret_access_key,
+    aws_session_token = aws_session_token,
+    region_name = region_name,
+    profile_name = profile_name
+  ) -> session
+
+  client <- session$client("athena")
+  res   <- client$get_query_execution(
+    QueryExecutionId = executionid)
+
+
+  # obtain the S3 output path
+  s3     <- session$client("s3")
+  s3path <- res$QueryExecution$ResultConfiguration$OutputLocation
+  s3path <- gsub("s3://", "", s3path)
+  bucket <- strsplit(s3path, "/")[[1]][[1]]
+  s3key  <- paste(
+    strsplit(s3path, "/")[[1]][-1],
+    collapse = "/")
+
+
+  # poll for the result; download when ready
+  print("Initializing retrieval of Athena Query.")
+  inittime <- Sys.time()
+
+  while(!exists('s3obj')) {
+
+    # use existence of the download object as the trigger
+    suppressWarnings(
+      try(s3obj <- s3$head_object(Bucket=bucket, Key=s3key)))
+
+    Sys.sleep(polling_duration)
+    duration <- Sys.time() - inittime
+    print(duration)
+
+    if (checktime==TRUE && abs(as.numeric(duration, units="mins"))  > maxtime ) {
+      stop("Query has exceeded the maximum specified time")
+    }
+
   }
 
+  print(paste("Query retrieval time (min): ",
+              abs(as.numeric(Sys.time() - inittime, units="mins"))))
 
+  if (exists("s3obj")) {
+    print(paste("downloading athena results file", s3path))
+    s3$download_file(Bucket = bucket, Key = s3key , Filename = local_filename)
+  }
+
+  local_filename
+
+  # localfile   <- save_query_results(
+  #   query_execution_id = executionid,
+  #   filename = local_filename,
+  #   aws_access_key_id = aws_access_key_id,
+  #   aws_secret_access_key = aws_secret_access_key,
+  #   aws_session_token = aws_session_token,
+  #   region_name = region_name,
+  #   profile_name = profile_name
+  # )
+  #
+  # future()
+  # region_name
+  #
+  # print("Initializing retrieval of Athena Query.")
+  # inittime <- Sys.time()
+  #
+  # while(!resolved(localfile)) {
+  #   Sys.sleep(polling_duration)
+  #   duration <- Sys.time() - inittime
+  #   print(duration)
+  #
+  #   if (checktime==TRUE && abs(as.numeric(duration, units="mins"))  > maxtime ) {
+  #     stop("Query has exceeded the maximum specified time")
+  #   }
+  # }
+  #
+  # print(paste("Query retrieval time (min): ",
+  #             abs(as.numeric(Sys.time() - inittime, units="mins"))))
+  #
+  # value(localfile)
 
 }
-
-
 
